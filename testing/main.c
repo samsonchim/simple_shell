e2231dfc9f32fc70c67fde4481f79f5ac9df70d1
Samson Chi <125648257+samsonchim@users.noreply.github.com>
2023-11-03 20:57:59 +0000

    Update main.c

--- testing/main.c
+++ testing/main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
-int main(int ac, char **argv){
-  char *prompt = "(Samson & Ricky) $ ";
-
-  printf("%s", prompt);
-  return (0);
+int main(void){
+    char *prompt = "(Samson & Ricky) $ ";
+    printf("%s", prompt);
+    return 0;
 }
